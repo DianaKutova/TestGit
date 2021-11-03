a833537f28ffb044fc6062e1e9a4f15bfea90a9c
Diana Kutova <diana.kutova@globallogic.com>
2021-11-03 17:18:55 +0200

    Added videTest.js file

---
+++ videoTest.js
@@ -0,0 +1,31 @@
+let myName = "Dianka"  ;
+let age = 23;
+
+// myName = "Diana";
+// age = 23;
+
+
+console.log(myName, age);
+
+const name = "Ді ";
+const dogName = 'Mania';
+
+console.log(typeof name,typeof dogName);
+
+function sayHello() {
+    let sms = "Hello JS" + " " + myName;
+    console.log(sms);
+
+}
+sayHello();
+
+
+function easyMath(a, b, c) {
+    let result = a+b/c;
+    console.log(result);
+    return result;
+}
+
+let sum = easyMath(77, 90, 3);
+
+console.log(sum);
